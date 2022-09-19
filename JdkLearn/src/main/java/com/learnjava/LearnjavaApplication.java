04ae4e8e6be8fa297c9c5c3834f27bcaf632b9b7
蔡福令 <41732517+chaifuling@users.noreply.github.com>
2022-09-19 11:06:03 +0800

    Update LearnjavaApplication.java
    fava  yyds

--- JdkLearn/src/main/java/com/learnjava/LearnjavaApplication.java
+++ JdkLearn/src/main/java/com/learnjava/LearnjavaApplication.java
@@ -1,14 +1,14 @@
-package com.learnjava;
+package com.learnfava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-public class LearnjavaApplication {
+public class LearnfavaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LearnjavaApplication.class, args);
+        SpringApplication.run(LearnfavaApplication.class, args);
     }
 
 }
